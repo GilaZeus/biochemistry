68f2ff56134ac67eea4459552f0c876634c9fa0c
Kiratsuwa <gilyazevr@gmail.com>
2020-07-08 02:00:11 +0200

    Fix the problem with drawing at the program start

--- program.py
+++ program.py
@@ -9,6 +9,10 @@ import chain_to_import
 def open_chain(path):
     '''Help to import and open a chain.'''
     shutil.copyfile(path, 'chain_to_import.py')
+
+def restore():
+    '''Restore the file to import before closing.'''
+    shutil.copyfile('logic\\chain_to_import.py', 'chain_to_import.py')
  
 class mywindow(QtWidgets.QMainWindow):
     def __init__(self):
@@ -21,6 +25,8 @@ class mywindow(QtWidgets.QMainWindow):
     def start_program(self):
         open_chain(self.ui.lineEdit.text())
         self.ui.second.show()
+        restore()
+
     
 
     def keyPressEvent(self, e):
--- test_turtle.py
+++
@@ -1,4 +0,0 @@
-from logic.turtle_helper import draw_postscript
-from chain_to_import import formate
-formate = formate.molecule
-draw_postscript(formate, 'test.png')
