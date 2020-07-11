305cf963a65f819efa9a4135920e0e67e317d234
Kiratsuwa <gilyazevr@gmail.com>
2020-07-11 17:22:58 +0200

    Fix windows' name, delete old scope statement

--- logic/gui/GUI.py
+++ logic/gui/GUI.py
@@ -118,6 +118,6 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "Biochemistry, 0.01-alpha"))
         for label in self.labels:
             label[0].setText(_translate("MainWindow", label[1]))
--- logic/gui/dialogue.py
+++ logic/gui/dialogue.py
@@ -49,6 +49,6 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "open file..."))
         self.pushButton.setText(_translate("MainWindow", "open"))
 
